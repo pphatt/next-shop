2e78033ac33dede3c9cb07571cb0e01dcf2dd4a3
pphatt <vutienphat0809@gmail.com>
2023-06-06 12:17:44 +0700

    feat(shop): add filter for product

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,11 +1,61 @@
 "use client";
-import React from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import styles from "@/styles/shop.module.scss";
 import NavigationBar from "@/components/navigation-bar";
 import { sixteen } from "@/test/products";
-import Footer from "@/components/footer"
+import Footer from "@/components/footer";
+import { useRouter } from "next/navigation";
 
-const Shop = () => {
+const Page = ({ searchParams }: { searchParams: {} }) => {
+  const [filter, setFilter] = useState<string[]>([]);
+
+  const [currentSortOption, setCurrentSortOption] = useState("Name: A-Z")
+  const [sortIsOpen, setSortIsOpen] = useState<boolean>(false);
+  const sortOptions = useMemo(
+    () => [
+      "Name: A-Z",
+      "Name: Z-A",
+      "Price: Decreased",
+      "Price: Increased",
+      "Newest",
+      "Oldest",
+    ],
+    []
+  );
+
+  const router = useRouter();
+  // console.log(filter);
+  // router.push();
+
+  const handleFilter = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (event.target.checked) {
+        setFilter([...filter, event.target.id]);
+      } else {
+        const tempFilter = [];
+
+        for (let i = 0; i < filter.length; i++) {
+          if (event.target.id !== filter[i]) {
+            tempFilter.push(filter[i]);
+          }
+        }
+
+        setFilter(tempFilter);
+      }
+    },
+    [filter]
+  );
+
+  // useEffect(() => {
+  //   // const filterQuery = ""
+  // }, [filter])
+
   const company = [
     "Good Smile Company",
     "Kotobukiya",
@@ -26,17 +76,56 @@ const Shop = () => {
           <header>Browse</header>
           <div className={styles["list-figure-wrapper"]}>
             <div className={styles["filter"]}>
+              <div className={styles["filter-sort"]}>
+                <span className={styles["filter-sort-title"]}>Sort by:</span>
+                <div
+                  className={styles["filter-box"]}
+                  onClick={() => setSortIsOpen(!sortIsOpen)}
+                >
+                  <div className={styles["filter-box-container"]}>
+                    <div className={styles["filter-option-holder"]}>
+                      <span>{currentSortOption}</span>
+                      <svg
+                        viewBox="0 0 10 7"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="15"
+                      >
+                        <path d="m0 .5 5 5 5-5H0Z"></path>
+                      </svg>
+                    </div>
+
+                    {sortIsOpen && (
+                      <ul className={styles["filter-options-dropdown"]}>
+                        {sortOptions.map((value, index) => (
+                          <li key={index} onClick={() => {
+                            setCurrentSortOption(value)
+                            setSortIsOpen(false)
+                          }}>{value}</li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
+                </div>
+              </div>
+
               <div className={styles["filter-section"]}>
                 <div className={styles["title"]}>Company</div>
                 <ul className={styles["filter-wrapper"]}>
                   {company.map((value, index) => (
                     <li key={index}>
                       <label htmlFor={value}>{value}</label>
-                      <input id={value} name={value} type={"checkbox"} />
+                      <input
+                        checked={filter.includes(value)}
+                        id={value}
+                        name={value}
+                        type={"checkbox"}
+                        onChange={(e) => handleFilter(e)}
+                      />
                     </li>
                   ))}
                 </ul>
               </div>
+
               <div className={styles["filter-section"]}>
                 <div className={styles["title"]}>Company</div>
                 <ul className={styles["filter-wrapper"]}>
@@ -53,42 +142,76 @@ const Shop = () => {
               <span>Display 10 of 100 figures</span>
               <div className={styles["divider"]}></div>
               <div className={styles["figure-list-container"]}>
-                {sixteen.map((value, index) => (
-                  <a key={index} className={styles["figure"]} href={"/"}>
-                    <div className={styles["poster-wrapper"]}>
-                      <div className={styles["poster-container"]}>
-                        <div className={styles["inner-poster-container"]}>
-                          <div>
-                            <picture className={styles["figure-image"]}>
-                              <source
-                                media="(min-width:768px)"
-                                srcSet={value.figure.image}
-                              />
-                              <img
-                                src={value.figure.image}
-                                alt=""
-                              />
-                            </picture>
-                            {value.figure.hover_image && (
-                              <picture className={styles["figure-image-hover"]}>
+                {sixteen.map((value, index) =>
+                  filter.length > 0 ? (
+                    filter.includes(value.figure.company) && (
+                      <a key={index} className={styles["figure"]} href={"/"}>
+                        <div className={styles["poster-wrapper"]}>
+                          <div className={styles["poster-container"]}>
+                            <div className={styles["inner-poster-container"]}>
+                              <div>
+                                <picture className={styles["figure-image"]}>
+                                  <source
+                                    media="(min-width:768px)"
+                                    srcSet={value.figure.image}
+                                  />
+                                  <img src={value.figure.image} alt="" />
+                                </picture>
+                                {value.figure.hover_image && (
+                                  <picture
+                                    className={styles["figure-image-hover"]}
+                                  >
+                                    <source
+                                      media="(min-width:768px)"
+                                      srcSet={value.figure.hover_image}
+                                    />
+                                    <img
+                                      src={value.figure.hover_image}
+                                      alt=""
+                                    />
+                                  </picture>
+                                )}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <span>{value.figure.name}</span>
+                        <span>6,250,000₫</span>
+                      </a>
+                    )
+                  ) : (
+                    <a key={index} className={styles["figure"]} href={"/"}>
+                      <div className={styles["poster-wrapper"]}>
+                        <div className={styles["poster-container"]}>
+                          <div className={styles["inner-poster-container"]}>
+                            <div>
+                              <picture className={styles["figure-image"]}>
                                 <source
                                   media="(min-width:768px)"
-                                  srcSet={value.figure.hover_image}
+                                  srcSet={value.figure.image}
                                 />
-                                <img
-                                  src={value.figure.hover_image}
-                                  alt=""
-                                />
+                                <img src={value.figure.image} alt="" />
                               </picture>
-                            )}
+                              {value.figure.hover_image && (
+                                <picture
+                                  className={styles["figure-image-hover"]}
+                                >
+                                  <source
+                                    media="(min-width:768px)"
+                                    srcSet={value.figure.hover_image}
+                                  />
+                                  <img src={value.figure.hover_image} alt="" />
+                                </picture>
+                              )}
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                    <span>{value.figure.name}</span>
-                    <span>6,250,000₫</span>
-                  </a>
-                ))}
+                      <span>{value.figure.name}</span>
+                      <span>6,250,000₫</span>
+                    </a>
+                  )
+                )}
               </div>
               <div className={styles["divider"]}></div>
               <div className={styles["pages-container"]}>
@@ -105,4 +228,4 @@ const Shop = () => {
   );
 };
 
-export default Shop;
+export default Page;
--- test/products.ts
+++ test/products.ts
@@ -57,8 +57,22 @@ const products: { figure: { name: string; image: string } }[] = [
   },
 ];
 
+// "Good Smile Company",
+//   "Kotobukiya",
+//   "Max Factory",
+//   "FREEing",
+//   "MegaHouse",
+//   "Bandai Spirit",
+//   "Phat Company",
+//   "Kadokawa",
+
 const sixteen: {
-  figure: { name: string; image: string; hover_image?: string };
+  figure: {
+    name: string;
+    image: string;
+    hover_image?: string;
+    company: string;
+  };
 }[] = [
   {
     figure: {
@@ -67,6 +81,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/vsinger_luo_tianyi_grain_in_ear_ver._1__1__d8c033b7490d49bb92ec423d5324f949_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/vsinger_luo_tianyi_grain_in_ear_ver._1__2__7ce156646b7a4d97a9c232b7b7b04ccf_master.jpg",
+      company: "Kadokawa",
     },
   },
   {
@@ -76,6 +91,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/i-oh__card_game_monster_figure_collection_eria_the_water_charmer_1__6__cd47723e89b649f19e6ff632801645d2_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/i-oh__card_game_monster_figure_collection_eria_the_water_charmer_1__8__28cc14b260c74f8a92c24b4619bfbcec_master.jpg",
+      company: "Kadokawa",
     },
   },
   {
@@ -85,6 +101,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/i-oh__card_game_monster_figure_collection_hiita_the_fire_charmer_1__2__15356952de204ffda4fd2b828af1c59d_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/i-oh__card_game_monster_figure_collection_hiita_the_fire_charmer_1__1__833b6372d6914909b4ddc4921d18f207_master.jpg",
+      company: "Kadokawa",
     },
   },
   {
@@ -94,6 +111,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/lycoris_recoil_takina_inoue_1__7__fd6a52effada40cea30fc85e3790f334_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/lycoris_recoil_takina_inoue_1__5__fdb27543eb2d41b5bb6e52146d066138_master.jpg",
+      company: "Kadokawa",
     },
   },
   {
@@ -103,6 +121,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/atelier_sophie_the_alchemist_of_the_mysterious_book_corneria_1__1__37fc74dc69f64fa7b034a3a269b64248_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/atelier_sophie_the_alchemist_of_the_mysterious_book_corneria_1__2__45247d40919c48d8b24306d12793d9ba_master.jpg",
+      company: "Bandai Spirit",
     },
   },
   {
@@ -112,6 +131,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/de_is_it_wrong_to_try_to_pick_up_girls_in_a_dungeon_iv_bell_cranel__6__52cd638fd39a4841b5092ce98517f101_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/de_is_it_wrong_to_try_to_pick_up_girls_in_a_dungeon_iv_bell_cranel__1__b7b8ff7eb47f4d22a193d45cd234efdf_master.jpg",
+      company: "Bandai Spirit",
     },
   },
   {
@@ -121,6 +141,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/figma_movie_jujutsu_kaisen_0_yuta_okkotsu__2__e2cee8ed1f694140b7371321e0fa881e_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/figma_movie_jujutsu_kaisen_0_yuta_okkotsu__3__6498faa454e048078a9c766c76782456_master.jpg",
+      company: "Bandai Spirit",
     },
   },
   {
@@ -130,6 +151,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/artfx_j_trigun_stampede_vash_the_stampede_trigun_stampede_ver._1__4__2598a427ef994870bf17f93827274810_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/artfx_j_trigun_stampede_vash_the_stampede_trigun_stampede_ver._1__5__2526a8ba24004497908247e1d7b4a400_master.jpg",
+      company: "Bandai Spirit",
     },
   },
   {
@@ -139,6 +161,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/tokyo_revengers_manjiro_sano_vol.24_cover_illustration_ver._1__1__3af95f0dd31e42dcab819044d4077aea_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/tokyo_revengers_manjiro_sano_vol.24_cover_illustration_ver._1__2__6c15fb16eb06402cb11585668f282fed_master.jpg",
+      company: "MegaHouse",
     },
   },
   {
@@ -148,6 +171,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/pop_up_parade_demon_slayer_kimetsu_no_yaiba_gyomei_himejima__6__3c534bcaa8124983886b64db7ba63391_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/pop_up_parade_demon_slayer_kimetsu_no_yaiba_gyomei_himejima__1__220be71108064fa285c3cac954266c5b_master.jpg",
+      company: "MegaHouse",
     },
   },
   {
@@ -157,6 +181,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/hatsune_miku_nt_style_casual_wear_ver__8__e55978667e05477aa11b379acb4461b0_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/hatsune_miku_nt_style_casual_wear_ver__2__038748ebd5dc4e0292eddee2b54acb09_master.jpg",
+      company: "MegaHouse",
     },
   },
   {
@@ -166,6 +191,7 @@ const sixteen: {
         "https://product.hstatic.net/1000160337/product/azur_lane_ark_royal_1.7_limited_edition__2__23a2e4f7b3f14ab187dec06c6fe001e5_master.jpg",
       hover_image:
         "https://product.hstatic.net/1000160337/product/azur_lane_ark_royal_1.7_limited_edition__4__ac07eb140ccf4ae388ebfd9aada313cc_master.jpg",
+      company: "MegaHouse",
     },
   },
 ];
